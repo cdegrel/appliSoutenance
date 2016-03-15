6aa12747c72ced57e84f7d030a480803289e5522
ThibaudPerrin <thibaudperrin@hotmail.fr>
2016-03-15 17:32:08 +0100

    Create nav.blade.php

---
+++ resources/views/jury/nav.blade.php
@@ -0,0 +1,9 @@
+<!-- Menu gauche -->
+<div id="contentNav">
+    <ul id="navigation">
+        <li class="accueil"><a href="" title="Accueil"><div></div></a></li>
+        <li class="planning"><a href="" title="Planning"><div></div></a></li>
+        <li class="options"><a href="" title="Options"><div></div></a></li>
+    </ul>
+</div>
+
